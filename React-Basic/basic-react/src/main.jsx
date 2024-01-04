6765eadbc71877823478cdac1ed6740878ebc439
Amit-priyu <priyuocta@gmail.com>
2024-01-04 23:06:02 +0530

    commit

--- React-Basic/basic-react/src/main.jsx
+++ React-Basic/basic-react/src/main.jsx
@@ -3,3 +3,6 @@ import { App } from './app.jsx'
 import './index.css'
 
 render(<App />, document.getElementById('app'))
+// ReactDOM.createRoot(document.getElementById('app')).render(
+//     <App />,
+// )
