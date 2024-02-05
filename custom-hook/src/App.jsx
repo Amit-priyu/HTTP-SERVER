4ac01683fedd6db3b7391816d5aee1f4ebec514c
Amit-priyu <priyuocta@gmail.com>
2024-02-05 13:48:36 +0530

    commit

--- custom-hook/src/App.jsx
+++ custom-hook/src/App.jsx
@@ -1,29 +1,42 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useIsOnline } from './hooks/useIsOnline';
+import { useDebounce } from './hooks/useDebounce';
 
 
-// defining our own custom hooks.
-function useInterval(fn,timeout){
-  useEffect(()=>{
-    setInterval(()=>{
-      fn()
-    },timeout)
-  },[])
-}
-function App() {
-  const [count,setCount]=useState(0);
-  useInterval(()=>{
-    setCount(c=>c+1);
-  },1000)
 
+
+function App(){
+    const [value,setValue]=useState(0);
+    const debouncedvalue=useDebounce(value,500);
   return (
     <>
-    Timer is at {count}
+     Debounced Value is {debouncedvalue};
+     <input type="text" onChange={e=>setValue(e.target.value)} />
     </>
   )
+}
+// defining our own custom hooks.
+// function useInterval(fn,timeout){
+//   useEffect(()=>{
+//     setInterval(()=>{
+//       fn()
+//     },timeout)
+//   },[])
+// }
+// function App() {
+//   const [count,setCount]=useState(0);
+//   useInterval(()=>{
+//     setCount(c=>c+1);
+//   },1000)
 
-} 
+//   return (
+//     <>
+//     Timer is at {count}
+//     </>
+//   )
+
+// } 
 
 function Track({ todo }) {
   return <div>
